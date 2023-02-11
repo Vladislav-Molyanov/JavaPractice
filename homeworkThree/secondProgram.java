dfd6796dee29aab5e03cc3af348735e93c08c4ad
Molyanov Vladislav <molyanov1488@gmail.com>
2023-02-12 02:55:59 +0400

    added problem solving to homeworkThree

--- homeworkThree/secondProgram.java
+++ homeworkThree/secondProgram.java
@@ -2,46 +2,39 @@
 
 package homeworkThree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class secondProgram {
+
+    private static Scanner input = new Scanner(System.in);
+    private static Random random = new Random();
     public static void main(String[] args) {
-        List  listNumbers = new ArrayList<Integer>();
-        listNumbers.add(1);
-        listNumbers.add(-2);
-        listNumbers.add(4);
-        listNumbers.add(-3);
-        listNumbers.add(33);
+        int len = getNumberByUser("\nВведите количество чисел списка: ");
+        List<Integer> numsList = new ArrayList<>();
+        fillList(numsList, len, 0, 20);
+        System.out.printf("\nИсходный список: \n%s", numsList);
 
-        deleteEvenElements(listNumbers);
-        System.out.println(listNumbers);
-
-        int min = (Integer)listNumbers.get(0);
-        int max = (Integer)listNumbers.get(0);
-        int lenList = listNumbers.size();
-        double sum = 0;
-        for (int i = 0; i < lenList; i++) {
-            if ((Integer) listNumbers.get(i)>0)
-                sum += (Integer) listNumbers.get(i);
-            else
-                sum += (Integer) listNumbers.get(i)*-1;
-            if ((Integer)listNumbers.get(i) < min) min = (Integer) listNumbers.get(i);
-            if ((Integer)listNumbers.get(i) > max) max = (Integer) listNumbers.get(i);
-        }
-
-        double srAr = sum/lenList;
-
-        System.out.printf("Максимальное = %d, Минимальное = %d\n", max, min);
-        System.out.print("Cреднее арифметичское = " + srAr);
+        filterOddNumsList(numsList);
+        System.out.printf("\nОтформатированный Список: \n%s", numsList);
     }
-    static List deleteEvenElements(List list){
-        for (int i = 0; i < list.size(); i++) {
-            if ((Integer)list.get(i) % 2 == 0){
-                list.remove(i);
+
+    public static int getNumberByUser(String text) {
+        System.out.print(text);
+        return input.nextInt();
+    }
+
+    public static void fillList(List<Integer> nums, int count, int min, int max) {
+        for (int i = 0; i < count; i++) {
+            nums.add(random.nextInt(count));
+        }
+    }
+
+    public static void filterOddNumsList(List<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 == 0) {
+                nums.remove(i);
                 i--;
             }
         }
-        return list;
     }
 }
